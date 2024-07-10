5e8acdc6e8eaa8a594346ced71ec746e21703035
shubhamagrl <s_agrawal@ph.iitr.ac.in>
2024-07-10 18:07:11 +0530

    Added Points Card

--- app/src/main/java/com/example/bdrapp/MainActivity.kt
+++ app/src/main/java/com/example/bdrapp/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
 import com.example.bdrapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(binding.root)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
         var arr = listOf(R.drawable.connected, R.drawable.disconnected)
         var index = 0
