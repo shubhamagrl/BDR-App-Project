babeded17d477a0da9f122625c8658e303987f2a
shubhamagrl <s_agrawal@ph.iitr.ac.in>
2024-07-10 14:49:05 +0530

    Added basic meter icon functionality.
    Tap to connect or disconnect

--- app/src/main/java/com/example/bdrapp/MainActivity.kt
+++ app/src/main/java/com/example/bdrapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.bdrapp
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +13,14 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        var image: ImageView = findViewById(R.id.idmeter)
+
+        var arr = listOf(R.drawable.connected, R.drawable.disconnected)
+        var index = 0
+
+        image.setOnClickListener{
+            index = (index+1)%2
+            image.setImageResource(arr[index])
+        }
     }
 }
