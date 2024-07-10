5501be2ccba05b6206ad847b5648bd5375be5bc7
shubhamagrl <s_agrawal@ph.iitr.ac.in>
2024-07-10 15:24:01 +0530

    Used data binding to add functionality

--- app/src/main/java/com/example/bdrapp/MainActivity.kt
+++ app/src/main/java/com/example/bdrapp/MainActivity.kt
@@ -6,21 +6,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.bdrapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        var image: ImageView = findViewById(R.id.idmeter)
+        setContentView(binding.root)
 
         var arr = listOf(R.drawable.connected, R.drawable.disconnected)
         var index = 0
 
-        image.setOnClickListener{
-            index = (index+1)%2
-            image.setImageResource(arr[index])
+        binding.apply {
+            idmeter.setOnClickListener {
+                index = (index + 1) % 2
+                idmeter.setImageResource(arr[index])
+            }
         }
     }
 }
