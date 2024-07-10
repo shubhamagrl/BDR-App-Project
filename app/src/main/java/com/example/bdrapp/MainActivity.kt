10d92328fb63409594f6a0ef965525469ba8e010
shubhamagrl <s_agrawal@ph.iitr.ac.in>
2024-07-10 12:06:20 +0530

    Added navigation bar

--- app/src/main/java/com/example/bdrapp/MainActivity.kt
+++ app/src/main/java/com/example/bdrapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.bdrapp
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,4 +18,6 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+    var mybtn: Button = findViewById(R.id.button)
+
 }
