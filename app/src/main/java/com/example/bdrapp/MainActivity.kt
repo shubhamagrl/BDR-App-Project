529b6741f615447a720ed6c219e15d3a415b91a0
Shubham Agrawal <agrlshubham@gmail.com>
2024-07-16 12:24:24 +0530

    Added Circular Progress Bar
    Added a menu for profile pages, and fragments for each page

--- app/src/main/java/com/example/bdrapp/MainActivity.kt
+++ app/src/main/java/com/example/bdrapp/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.bdrapp
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import android.widget.ImageView
+import android.widget.PopupMenu
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,6 +22,49 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
+        val profile_button: TextView = findViewById(R.id.idprofile)
+        profile_button.setOnClickListener { view ->
+            showPopupMenu(view)
+        }
+
+        meter()
+        displayProgress()
+    }
+
+    fun showPopupMenu(view: View) {
+        val popupMenu = PopupMenu(this, view)
+        popupMenu.menuInflater.inflate(R.menu.profile_menu, popupMenu.menu)
+
+        popupMenu.setOnMenuItemClickListener { menuItem ->
+            handleMenuItemClick(menuItem)
+        }
+
+        popupMenu.show()
+    }
+    private fun handleMenuItemClick(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.option_1 -> {
+                Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.option_2 -> {
+                Toast.makeText(this, "Add Account", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.option_3 -> {
+                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> false
+        }
+    }
+
+
+    fun displayProgress(){
+        val progressValue = 60
+        binding.idprogressbar.progress = progressValue
+    }
+    fun meter(){
         var arr = listOf(R.drawable.connected, R.drawable.disconnected)
         var index = 0
 
@@ -26,12 +74,5 @@ class MainActivity : AppCompatActivity() {
                 idmeter.setImageResource(arr[index])
             }
         }
-        displayProgress()
-    }
-    fun displayProgress(){
-        val progressValue = 40
-        binding.idprogressbar.progress = progressValue
-
-        binding.idprogress.text = progressValue.toString()
     }
 }
