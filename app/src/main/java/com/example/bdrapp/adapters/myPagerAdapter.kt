373867525cb1d41340c1ce92af7ecb67b85b69b3
shubhamagrl <s_agrawal@ph.iitr.ac.in>
2024-07-17 11:26:39 +0530

    Made more changes in the Tab Layout

--- app/src/main/java/com/example/bdrapp/adapters/myPagerAdapter.kt
+++ app/src/main/java/com/example/bdrapp/adapters/myPagerAdapter.kt
@@ -3,21 +3,25 @@ package com.example.bdrapp.adapters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import androidx.lifecycle.Lifecycle
-import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class myPagerAdapter(fm: FragmentManager, lc: Lifecycle): FragmentStateAdapter(fm,lc){
+class myPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm,
+    BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
+){
 
-    var FragmentsList: ArrayList<Fragment> = ArrayList()
-
-    fun addFragments(fragment: Fragment){
-        FragmentsList.add(fragment)
-    }
-    override fun getItemCount(): Int {
-        return FragmentsList.size
+    private val fragmentsList = ArrayList<Fragment>()
+    private val fragmentsTitleList = ArrayList<String>()
+    override fun getCount(): Int {
+        return fragmentsList.size
     }
 
-    override fun createFragment(position: Int): Fragment {
-        return FragmentsList.get(position)
+    override fun getItem(position: Int): Fragment {
+        return fragmentsList[position]
+    }
+    override fun getPageTitle(position: Int): CharSequence? {
+        return fragmentsTitleList[position]
+    }
+    fun addFragments(fragment: Fragment, title: String){
+        fragmentsList.add(fragment)
+        fragmentsTitleList.add(title)
     }
 }
